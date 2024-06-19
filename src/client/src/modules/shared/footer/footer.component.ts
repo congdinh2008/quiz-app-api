f3e257a79bae6a1bf0b6149fb0f85bec4cdcc0de
Cong Dinh <congdinh2012@hotmail.com>
2024-06-19 14:56:38 +0700

    Implement pipe

--- src/client/src/modules/shared/footer/footer.component.ts
+++ src/client/src/modules/shared/footer/footer.component.ts
@@ -1,12 +1,13 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  public owner: string = 'ViVu Store';  
 }
